9f71eb262c194d80420ca082fbae8bd9c9af745b
Willyan Ferdinando <willyantferdinando@gmail.com>
2020-07-05 14:29:18 -0300

    Index Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   get 'welcome/index'
+ #get '/inicio', to: 'welcome#index'
+  root to: 'welcome#index'
+  
   resources :coins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  
+
 end
