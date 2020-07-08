940969761bda2bf55a72826be441271a6a5a02d5
Willyan Ferdinando <willyantferdinando@gmail.com>
2020-07-07 22:13:38 -0300

    Refatoramento Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,24 +6,31 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-puts 'Cadsatrando as moedas......'
+spinner = TTY::Spinner.new("[:spinner] Cadastrando Moedas")
+spinner.auto_spin
 
-Coin.create!(
-  description: 'Bitcoin',
-  acronym: 'BTC',
-  url_image:
-    'https://toppng.com/uploads/preview/bitcoin-png-bitcoin-logo-transparent-background-11562933997uxok6gcqjp.png'
-)
-Coin.create!(
-  description: 'Ethereum',
-  acronym: 'ETH',
-  url_image:
-    'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/ETHEREUM-YOUTUBE-PROFILE-PIC.png/600px-ETHEREUM-YOUTUBE-PROFILE-PIC.png'
-)
-Coin.create!(
-  description: 'Dash',
-  acronym: 'DASH',
-  url_image:
-    'https://www.zenithincome.com/uploads/currencies/icon/156518530383Dash-D-white_on_blue_circle.png'
-)
-puts 'Moedas cadastradas com sucesso!......'
+coins = [
+  {
+    description: 'Bitcoin',
+    acronym: 'BTC',
+    url_image:'https://toppng.com/uploads/preview/bitcoin-png-bitcoin-logo-transparent-background-11562933997uxok6gcqjp.png'
+  },
+
+  {
+    description: 'Ethereum',
+    acronym: 'ETH',
+    url_image:'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/ETHEREUM-YOUTUBE-PROFILE-PIC.png/600px-ETHEREUM-YOUTUBE-PROFILE-PIC.png'
+  },
+
+  {
+    description: 'Dash',
+    acronym: 'DASH',
+    url_image:'https://www.zenithincome.com/uploads/currencies/icon/156518530383Dash-D-white_on_blue_circle.png'
+  }
+]
+
+coins.each do|coin|
+  sleep(0.5)
+  Coin.find_or_create_by!(coin)
+end
+spinner.success("(Concluído com sucesso!)")
