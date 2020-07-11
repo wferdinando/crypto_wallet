a1d9d8e871c4ed4a1fbbb860a77097a50abcd540
Willyan Ferdinando <willyantferdinando@gmail.com>
2020-07-11 00:20:06 -0300

    I18n changes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    session[:user_name] = "Curso de Ruby on Rails [SESSION]"
+    cookies[:curso] = "Curso de Ruby on Rails [COOKIE]"
     @nome = params[:nome]
   end
 end
