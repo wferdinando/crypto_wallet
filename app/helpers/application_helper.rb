7f89a9689ec048ccb6ed80c223be909748682091
Willyan Ferdinando <willyantferdinando@gmail.com>
2020-07-10 21:02:22 -0300

    Add I18n

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 module ApplicationHelper
+  def locale
+    I18n.locale == :en ? "Inglês Americano" : "Português do Brasil"
+  end
+
   def data_br(data_us)
     data_us.strftime('%d/%m/%Y')
   end
@@ -6,4 +10,4 @@ module ApplicationHelper
   def nome_aplicacao
     'CRYPTO WALLET APP'
   end
-end
+end 
