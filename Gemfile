ffa5d7eb2dd975ba6b83d9e4c4f30a7b18e2eed4
Willyan Ferdinando <willyantferdinando@gmail.com>
2020-07-12 00:14:25 -0300

    Finalização

--- Gemfile
+++ Gemfile
@@ -40,6 +40,7 @@ gem 'pry-rails'
 gem 'tty-spinner'
 gem 'rails-i18n', '~> 5.1' # For 5.0.x, 5.1.x and 5.2.x
 
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,13 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+Rails.application.config.assets.precompile += %w( coins.css 
+                                                  scaffolds.css
+                                                  mining_types.css
+                                                  welcome.css)
+
+Rails.application.config.assets.precompile += %w( cable.js
+                                                  coins.js 
+                                                  mining_types.js
+                                                  welcome.js)                                                 
